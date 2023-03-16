b5c0e75f15e3fb5cd5abeb77fc22a088e8bb03cc
Jamie Thompson <bishbashboshjt@gmail.com>
2023-03-16 15:01:21 +0100

    parse proposed toml syntax into config object

--- src/main/scala/builder/Config.scala
+++ src/main/scala/builder/Config.scala
@@ -1,16 +1,98 @@
 package builder
 
-trait SharedConfig:
-  def name: Option[String]
+import toml.Parse
+
+import scala.util.control.NonLocalReturns.*
+
+object Config:
+  import toml.Codecs.{*, given}
+  import toml.Value.*
+
+  def parse(text: String): Either[String, Config] =
+    toml.Toml.parse(text) match
+      case Left((_, msg)) => Left(msg)
+      case Right(value) => readConfig(value)
+
+  private def readConfig(table: Tbl): Either[String, Config] = returning {
+    Right(Config(
+      scalaVersion = table.values.get("scalaVersion").flatMap({
+        case Str(value) => Some(value)
+        case _ => throwReturn(Left("scalaVersion must be a string"))
+      }),
+      modules = table.values.get("modules").flatMap({
+        case Tbl(values) =>
+          val parsed = values.toList.map((k, v) => readModule(k, v) match
+            case Left(err) => throwReturn(Left(err))
+            case Right(value) => value
+          )
+          val asSet = parsed.toSet
+          val validated = parsed.map(module => validateModule(module, asSet - module) match
+            case Left(err) => throwReturn(Left(err))
+            case Right(_) => ()
+          )
+          Some(parsed)
+        case _ => throwReturn(Left("modules must be a table"))
+      }).getOrElse(Nil)
+    ))
+  }
+
+  private def validateModule(module: Module, rest: Set[Module]): Either[String, Unit] =
+    if module.dependsOn.contains(module.name) then
+      Left(s"module ${module.name} cannot depend on itself")
+    else if module.kind == ModuleKind.Application && rest.exists(_.dependsOn.contains(module)) then
+      Left(s"module ${module.name} is depended on by ${rest.find(_.dependsOn.contains(module)).get.name} but is an application")
+    else if module.dependsOn.exists(mod => !rest.exists(_.name == mod)) then
+      Left(s"module ${module.name} depends on ${module.dependsOn.find(mod => !rest.exists(_.name == mod)).get} which does not exist")
+    else
+      Right(())
+
+  private def readModule(key: String, value: toml.Value): Either[String, Module] = returning {
+    value match
+      case Tbl(values) =>
+        val name = values.get("name").flatMap({
+          case Str(value) => Some(value)
+          case _ => throwReturn(Left(s"modules.${key}.name must be a string"))
+        }).getOrElse(key)
+        val root = values.get("root").flatMap({
+          case Str(value) => Some(value)
+          case _ => throwReturn(Left(s"modules.${key}.root must be a string"))
+        }).getOrElse(key)
+        val kind = values.get("kind").flatMap({
+          case Str(value) => Some(value)
+          case _ => throwReturn(Left(s"modules.${key}.kind must be a string"))
+        }).getOrElse("library")
+        val dependsOn = values.get("dependsOn").flatMap({
+          case Arr(values) => Some(values.flatMap({
+            case Str(value) => Some(value)
+            case _ => throwReturn(Left(s"modules.${key}.dependsOn must be a list of strings"))
+          }))
+          case _ => throwReturn(Left(s"modules.${key}.dependsOn must be a list of strings"))
+        }).getOrElse(Nil)
+        Right(Module(
+          name = name,
+          root = root,
+          kind = kind match
+            case "library" => ModuleKind.Library
+            case "application" => ModuleKind.Application
+            case "resource" => ModuleKind.Resource
+            case _ => throwReturn(Left(s"unknown module kind for modules.${key}.kind: $kind")),
+          dependsOn = dependsOn
+        ))
+      case _ =>
+        Left(s"module.$key must be a table")
+  }
 
 case class Config(
-  name: Option[String] = None,
+  scalaVersion: Option[String] = None,
   modules: List[Module] = Nil
-) extends SharedConfig
-
+)
 
 case class Module(
-  name: Option[String] = None,
-  // version: Option[String] = None,
-  // dependencies: List[Dependency] = Nil
-) extends SharedConfig
+  name: String,
+  root: String,
+  kind: ModuleKind = ModuleKind.Library,
+  dependsOn: List[String] = Nil
+)
+
+enum ModuleKind:
+  case Library, Application, Resource
---
+++ src/test/scala/builder/ConfigTest.scala
@@ -0,0 +1,51 @@
+package builder
+
+val exampleFullStackAppConf = """
+scalaVersion = "3.2.2"
+
+[modules.webserver]
+kind = "application" # cannot be depended on by other modules
+dependsOn = ["core", "webpage"]
+
+[modules.webpage]
+kind = "resource" # dependends depend on the packaged output
+dependsOn = ["core"]
+
+[modules.core]
+kind = "library" # dependents depend on the classpath output
+"""
+
+class ConfigTest extends munit.FunSuite {
+
+  test("parse config from valid toml") {
+
+    val config = Config.parse(exampleFullStackAppConf) match
+      case Left(err) => fail(err)
+      case Right(value) => value
+
+    assertEquals(config, Config(
+      scalaVersion = Some("3.2.2"),
+      modules = List(
+        Module(
+          name = "webserver",
+          root = "webserver",
+          kind = ModuleKind.Application,
+          dependsOn = List("core", "webpage")
+        ),
+        Module(
+          name = "webpage",
+          root = "webpage",
+          kind = ModuleKind.Resource,
+          dependsOn = List("core")
+        ),
+        Module(
+          name = "core",
+          root = "core",
+          kind = ModuleKind.Library,
+          dependsOn = Nil
+        )
+      )
+    ))
+  }
+
+}
